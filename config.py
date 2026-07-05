db581b20f6e55539cfd5eef0de7c5c93411bd2c6
isholaqowiy <isholaqowiy8@gmail.com>
2026-07-05 03:01:43 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+TEMP_DIR = "temp_signatures"
+DB_NAME = "signflow.db"
+
